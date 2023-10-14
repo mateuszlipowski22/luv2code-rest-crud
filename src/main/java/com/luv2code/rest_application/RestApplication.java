4b5b0f443a2c4fc4788ce87703b46ecc08746bbb
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-10-14 22:45:00 +0200

    Say hello rest controller

--- src/main/java/com/luv2code/rest_application/RestApplication.java
+++ src/main/java/com/luv2code/rest_application/RestApplication.java
@@ -1,4 +1,4 @@
-package com.luv2code.rest;
+package com.luv2code.rest_application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
