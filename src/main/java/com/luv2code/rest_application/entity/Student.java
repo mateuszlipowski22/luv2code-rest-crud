ef1b0c931f64570b84475918a384bf81a9d0efc0
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-10-15 22:40:14 +0200

    Adding lombok, Student class and StudentRestController

---
+++ src/main/java/com/luv2code/rest_application/entity/Student.java
@@ -0,0 +1,15 @@
+package com.luv2code.rest_application.entity;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+    private String firstName;
+    private String lastName;
+
+}
---
+++ src/main/java/com/luv2code/rest_application/rest/StudentRestController.java
@@ -0,0 +1,25 @@
+package com.luv2code.rest_application.rest;
+
+import com.luv2code.rest_application.entity.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+
+        List<Student> theStudents = new ArrayList<>();
+        theStudents.add(new Student("Jan", "Nowak"));
+        theStudents.add(new Student("Marek", "Kowalski"));
+        theStudents.add(new Student("Zbigniew", "Nowak"));
+        return theStudents;
+
+    }
+}
