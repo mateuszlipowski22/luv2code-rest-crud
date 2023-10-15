c98b9a9f9c967ed69c87866da186f157c07ada2a
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-10-15 23:05:42 +0200

    Adding getStudent by id endpoint

--- src/main/java/com/luv2code/rest_application/rest/StudentRestController.java
+++ src/main/java/com/luv2code/rest_application/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.luv2code.rest_application.rest;
 
 import com.luv2code.rest_application.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,14 +14,23 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents(){
+    List<Student> theStudents;
 
-        List<Student> theStudents = new ArrayList<>();
+    @PostConstruct
+    public void loadData(){
+        theStudents = new ArrayList<>();
         theStudents.add(new Student("Jan", "Nowak"));
         theStudents.add(new Student("Marek", "Kowalski"));
         theStudents.add(new Student("Zbigniew", "Nowak"));
-        return theStudents;
+    }
 
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        return theStudents;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+        return theStudents.get(studentId);
     }
 }
