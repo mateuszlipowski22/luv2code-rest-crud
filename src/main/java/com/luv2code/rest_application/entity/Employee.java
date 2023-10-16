4851a8754c126b4ef30e504fc16cca1f9235a389
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-10-16 22:59:21 +0200

    Adding Emplyee.class entity

---
+++ src/main/java/com/luv2code/rest_application/entity/Employee.java
@@ -0,0 +1,29 @@
+package com.luv2code.rest_application.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="employee")
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email")
+    private String email;
+
+}
