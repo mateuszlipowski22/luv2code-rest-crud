a8cdcdadde235b69f6739ebbe991f0c11a0c4935
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-10-19 22:27:25 +0200

    Adding spring.data.rest.default-page-size

--- src/main/java/com/luv2code/rest_application/dao/EmployeeRepository.java
+++ src/main/java/com/luv2code/rest_application/dao/EmployeeRepository.java
@@ -2,8 +2,7 @@ package com.luv2code.rest_application.dao;
 
 import com.luv2code.rest_application.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "employees", path = "employees")
+//@RepositoryRestResource(collectionResourceRel = "employees", path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
