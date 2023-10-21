333a00062cdafebf35e506809f038e5ffa253092
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-10-21 22:50:16 +0200

    Adding restricted access based on roles

--- src/main/java/com/luv2code/rest_application/security/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/rest_application/security/DemoSecurityConfig.java
@@ -2,9 +2,13 @@ package com.luv2code.rest_application.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -31,5 +35,23 @@ public class DemoSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john,mary,susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+
+        httpSecurity.httpBasic(Customizer.withDefaults());
+
+        httpSecurity.csrf(csrf->csrf.disable());
+
+        return httpSecurity.build();
     }
 }
