8ad6a24ab90fa562c60accdd21904c26f7c0fc8f
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-10-30 22:15:03 +0100

    Adding InitBinder for string trimming

--- src/main/java/com/luv2code/rest_application/controller/CustomerController.java
+++ src/main/java/com/luv2code/rest_application/controller/CustomerController.java
@@ -2,15 +2,24 @@ package com.luv2code.rest_application.controller;
 
 import com.luv2code.rest_application.model.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder){
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @GetMapping("/customer/")
     public String showForm(Model model){
@@ -24,6 +33,7 @@ public class CustomerController {
             @Valid @ModelAttribute("customer") Customer customer,
             BindingResult bindingResult){
 
+        System.out.println("Last name : ["+customer.getLastName()+"]");
         if(bindingResult.hasErrors()){
             return "customer-form";
         }else {
