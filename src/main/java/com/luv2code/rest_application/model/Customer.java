1d15632b0bae68e2e3b333ad25e7650db90d65e0
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-10-30 22:35:04 +0100

    Adding custom message for invalid type

--- src/main/java/com/luv2code/rest_application/model/Customer.java
+++ src/main/java/com/luv2code/rest_application/model/Customer.java
@@ -16,9 +16,10 @@ public class Customer {
     @Size(min=1, message = "is required")
     private String lastName="";
 
+    @NotNull(message = "is required")
     @Min(value=0, message = "Must be grater than or equal to 0")
     @Max(value=10, message = "Must be less than ot equal to 10")
-    private int freePasses;
+    private Integer freePasses;
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars / digits")
     private String postalCode;
