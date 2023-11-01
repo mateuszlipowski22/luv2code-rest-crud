936665762fe2d8d1b909ae98669ea8074a8943a5
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-11-01 22:33:46 +0100

    Adding "back to employee list" button and improvement of sorted list

--- src/main/java/com/luv2code/rest_application/dao/EmployeeRepository.java
+++ src/main/java/com/luv2code/rest_application/dao/EmployeeRepository.java
@@ -3,6 +3,11 @@ package com.luv2code.rest_application.dao;
 import com.luv2code.rest_application.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 //@RepositoryRestResource(collectionResourceRel = "employees", path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    public List<Employee> findAllByOrderByLastNameAsc();
+
 }
