cef8c64a72d6eb3c4eebe3180870f3cba161dd33
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-11-07 23:49:57 +0100

    adding database connection, and commandrunner

--- src/main/java/com/luv2code/rest_application/RestApplication.java
+++ src/main/java/com/luv2code/rest_application/RestApplication.java
@@ -1,7 +1,9 @@
 package com.luv2code.rest_application;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RestApplication {
@@ -10,4 +12,10 @@ public class RestApplication {
 		SpringApplication.run(RestApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args){
+		return runner ->{
+			System.out.println("Hello World");
+		};
+	}
 }
